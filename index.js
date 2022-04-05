b5250214087ab45ee8492c12f1d480b121b4a3b5
Vikrant <vikrant.kumar@aryavratinfotech.com>
2022-04-05 12:45:01 +0530

    sharefront

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://viku-share.herokuapp.com/";
+const baseURL = "https://viku-share.herokuapp.com";
 // const baseURL = "http://192.168.1.140:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
